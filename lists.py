f5aede97d856a8022ef2af97475fb14701b49b47
Your Name <you@example.com>
2024-07-11 10:58:55 +0100

    added lists

---
+++ lists.py
@@ -0,0 +1,20 @@
+coffee_order = [
+    "Alex - Flat White",
+    "Emily - Hot Chocolate",
+    "Alice - Black Coffee"
+]
+# print(coffee_order)
+# print(coffee_order[2])
+# print(coffee_order[1])
+# coffee_order[1] = "Steve - Latte"
+# print(len(coffee_order))
+# print(len(coffee_order[1]))
+
+
+# print(coffee_order)
+# coffee_order.append("Adam - Water")
+# print(coffee_order)
+
+print(coffee_order)
+coffee_order.pop()
+print(coffee_order)
